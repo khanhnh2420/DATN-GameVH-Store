4a5537c962462eaab57788603001e20184aec17c
Khanh <hoangkhanh2420@gmail.com>
2023-05-17 16:19:08 +0700

    fix upload product

--- DATN_GameVH/src/main/resources/static/admin/js/product-list.js
+++ DATN_GameVH/src/main/resources/static/admin/js/product-list.js
@@ -1,14 +1,36 @@
 function previewImage(event) {
   var input = event.target;
   var reader = new FileReader();
+  var imgPreview = document.getElementById("img-preview");
 
   reader.onload = function () {
-    var imgPreview = document.getElementById("img-preview");
+    imgPreview.classList.add("img-thumbnail");
     imgPreview.src = reader.result;
   };
 
-  reader.readAsDataURL(input.files[0]);
+  if (input.files && input.files[0]) {
+    reader.readAsDataURL(input.files[0]);
+  } else {
+    imgPreview.src = "img/new-product/No-Image-poster.png";
+  }
 }
+
+const toast = document.querySelector(".toast")
+      closeIcon = document.querySelector(".close"),
+      progress = document.querySelector(".progress");
+
+let timer1, timer2;
+
+closeIcon.addEventListener("click", () => {
+  toast.classList.remove("active");
+
+  setTimeout(() => {
+    progress.classList.remove("active");
+  }, 4000);
+
+  clearTimeout(timer1);
+  clearTimeout(timer2);
+});
 
 function previewImages(event) {
   var input = event.target;
@@ -17,7 +39,18 @@ function previewImages(event) {
 
   // Kiểm tra số lượng file đã chọn và thông báo nếu vượt quá giới hạn
   if (input.files.length > 4) {
-    alert("Vui lòng chọn tối đa 4 tệp.");
+    
+    toast.classList.add("active");
+    progress.classList.add("active");
+  
+    timer1 = setTimeout(() => {
+      toast.classList.remove("active");
+    }, 5000); //1s = 1000 milliseconds
+  
+    timer2 = setTimeout(() => {
+      progress.classList.remove("active");
+    }, 5300);
+
     input.value = ""; // Xóa tất cả các tệp đã chọn
     previewList.innerHTML = ""; // Xóa các ảnh đã hiển thị
     return;
@@ -26,7 +59,14 @@ function previewImages(event) {
   var fileCount = Math.min(input.files.length, 4); // Chỉ xử lý tối đa 4 file
 
   if (fileCount === 0) {
-    return; // Không có file nào được chọn, thoát khỏi hàm
+    // Hiển thị hình ảnh mặc định khi không có file nào được chọn
+    for (var i = 0; i < 4; i++) {
+      var placeholderImg = document.createElement("img");
+      placeholderImg.src = "img/new-product/no-image.jpg";
+      placeholderImg.classList.add("img-thumbnail");
+      previewList.appendChild(placeholderImg);
+    }
+    return;
   }
 
   for (var i = 0; i < fileCount; i++) {
@@ -34,15 +74,17 @@ function previewImages(event) {
 
     reader.onload = function (e) {
       var imgContainer = document.createElement("div");
-      imgContainer.classList.add("thumbnail");
+      imgContainer.classList.add("thumbnailPic");
       previewList.appendChild(imgContainer);
 
       var img = document.createElement("img");
+      img.classList.add("img-thumbnail");
       img.src = e.target.result;
       imgContainer.appendChild(img);
     };
 
     reader.readAsDataURL(input.files[i]);
   }
-
 }
+
+

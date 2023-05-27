74a28da6fdb692f6f76f3a58d714e8be0830ddb3
KIT2420 <hoangkhanh2420@gmail.com>
2023-05-27 21:23:51 +0700

    up

--- DATN_GameVH/src/main/java/com/fourTL/controller/admin/Account/AccountsRestAPI.java
+++ DATN_GameVH/src/main/java/com/fourTL/controller/admin/Account/AccountsRestAPI.java
@@ -1,8 +1,12 @@
-package com.fourTL.controller.admin.Acountmanagement;
+package com.fourTL.controller.admin.Account;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fourTL.dao.AccountsDAO;
@@ -22,8 +27,8 @@ import com.fourTL.entities.Categories;
 
 @CrossOrigin("*")
 @RestController
-@RequestMapping("/admin/api/acount")
-public class AcountController {
+@RequestMapping("/admin/api/accounts")
+public class AccountsRestAPI {
 	@Autowired
 	AccountsDAO accountsDAO;
 
@@ -31,13 +36,12 @@ public class AcountController {
 	CategoriesDAO categoriesDAO;
 	
 	@GetMapping("")
-	public ResponseEntity<List<Accounts>> getAll(Model model) {
-		return ResponseEntity.ok(accountsDAO.findAll());
-	}
-
-	@GetMapping("/categories")
-	public ResponseEntity<List<Categories>> getAllCategories(Model model) {
-		return ResponseEntity.ok(categoriesDAO.findAll());
+	public ResponseEntity<Page<Accounts>> getAllAccounts(
+			@RequestParam("page") Optional<Integer> page, 
+			@RequestParam("size") Optional<Integer> size) {
+		Pageable pageable = PageRequest.of(page.orElse(0), size.orElse(6));
+		Page<Accounts> accounts = accountsDAO.findAll(pageable);
+		return ResponseEntity.ok(accounts);
 	}
 	
 	@GetMapping("/search/{search}")

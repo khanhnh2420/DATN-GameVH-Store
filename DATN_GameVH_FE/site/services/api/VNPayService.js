fd27e49ba972c4a9d10f2d564e8699d60f5be473
NguyenHan <handev2607@gmail.com>
2023-07-13 22:34:49 +0700

    Hân
    up code fix src image các trang site phù hợp với image trên google drive

--- DATN_GameVH_FE/site/services/api/VNPayService.js
+++ DATN_GameVH_FE/site/services/api/VNPayService.js
@@ -1,7 +1,8 @@
 app.factory('VNPayService', function ($http) {
+    var baseUrl = host + '/api/vnpay';
     return {
         createOrder: function (totalPrice, userIP) {
-            return $http.get('http://localhost:8080/api/vnpay/create/' + totalPrice + '/' + userIP);
+            return $http.get(baseUrl + '/create/' + totalPrice + '/' + userIP);
         }
     }
 });

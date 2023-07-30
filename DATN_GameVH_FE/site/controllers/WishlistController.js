59ae90514e4d0bc8c8fd9af68b729ad8e0368c06
huypro <92622056+huypro-chet@users.noreply.github.com>
2023-07-31 05:25:02 +0700

    them danh sach yeu thich vao trang shop

--- DATN_GameVH_FE/site/controllers/WishlistController.js
+++ DATN_GameVH_FE/site/controllers/WishlistController.js
@@ -1,4 +1,4 @@
-app.controller("WishlistController", function ($scope, ToastService, AccountService, ProductService, WishlistService, $window, $http) {
+app.controller("WishlistController", function($scope, ToastService, AccountService, ProductService, WishlistService, $window, $http) {
     $scope.favorite = []
 
     $scope.account = {}; // Biến lưu thông tin account 
@@ -7,7 +7,7 @@ app.controller("WishlistController", function ($scope, ToastService, AccountServ
 
 
 
-    $scope.logout = function () {
+    $scope.logout = function() {
         $window.localStorage.removeItem("username");
         $window.sessionStorage.removeItem("username");
         $window.sessionStorage.setItem("messageLogin", "Bạn đã đăng xuất!");
@@ -17,11 +17,11 @@ app.controller("WishlistController", function ($scope, ToastService, AccountServ
     var link = $window.location.href || '';
 
     if (link.indexOf("wishlist") !== -1) {
-        AccountService.checkLogin().then(function (account) {
+        AccountService.checkLogin().then(function(account) {
             // Người dùng đã đăng nhập
             $scope.account = account;
             $scope.fetchWishlistItems();
-        }).catch(function (error) {
+        }).catch(function(error) {
             // Người dùng chưa đăng nhập hoặc có lỗi
             console.error('Lỗi đăng nhập hoặc chưa đăng nhập:', error);
             if (error === "Người dùng chưa đăng nhập") {
@@ -35,31 +35,31 @@ app.controller("WishlistController", function ($scope, ToastService, AccountServ
     }
 
     $scope.totalFavorite = 0;
-    $scope.fetchWishlistItems = function () {
+    $scope.fetchWishlistItems = function() {
         $scope.totalFavorite = 0;
         if ($scope.account && $scope.account.id) {
             var accountId = $scope.account.id;
             // Gọi WishlistService để lấy danh sách yêu thích
             WishlistService.getWishlist(accountId)
-                .then(function (response) {
+                .then(function(response) {
                     // Gán dữ liệu danh sách yêu thích vào biến $scope.favorite
                     $scope.favorite = response.data;
-                    $scope.favorite.forEach(function (item) {
+                    $scope.favorite.forEach(function(item) {
                         if (item.status) {
                             $scope.totalFavorite++;
                         }
                     })
                     document.getElementById("favoriteLength").innerText = $scope.totalFavorite;
                 })
-                .catch(function (error) {
+                .catch(function(error) {
                     console.error('Lỗi khi lấy danh sách yêu thích:', error);
                 });
         }
     }
 
 
-    $scope.toggleFavorite = function (event) {
-        AccountService.checkLogin().then(function (account) {
+    $scope.toggleFavorite = function(event) {
+        AccountService.checkLogin().then(function(account) {
             // Người dùng đã đăng nhập
             $scope.account = account;
             if ($scope.account) {
@@ -80,24 +80,24 @@ app.controller("WishlistController", function ($scope, ToastService, AccountServ
                     existingFavorite.status = !existingFavorite.status; // Đảo ngược trạng thái yêu thích
 
                     WishlistService.updateFavorite(existingFavorite)
-                        .then(function (response) {
+                        .then(function(response) {
                             // Xử lý phản hồi từ máy chủ nếu cần
                             console.log('Cập nhật trạng thái sản phẩm thành công.');
 
                             // Thông báo sản phẩm thành công khi cập nhật từ true sang false
                             if (existingFavorite.status === true) {
-                                showSuccessToast("Sản phẩm đã thêm vào danh sách thành công");
+                                showSuccessToast("Sản phẩm đã thêm vào danh sách yêu thích thành công");
                             } else {
                                 showErrorToast("Sản phẩm đã tồn tại");
                             }
                         })
-                        .catch(function (error) {
+                        .catch(function(error) {
                             console.error('Lỗi khi cập nhật sản phẩm yêu thích:', error);
                         });
                 } else {
                     // Sản phẩm chưa tồn tại trong danh sách yêu thích, thêm mới vào
                     ProductService.getProduct(productId)
-                        .then(function (response) {
+                        .then(function(response) {
                             var product = response.data;
                             if (product.available && product.qty > 0) {
                                 var favoriteData = {
@@ -111,26 +111,26 @@ app.controller("WishlistController", function ($scope, ToastService, AccountServ
                                 };
 
                                 WishlistService.addWishlist(favoriteData)
-                                    .then(function (response) {
+                                    .then(function(response) {
                                         // Xử lý phản hồi từ máy chủ nếu cần
                                         console.log('Thêm sản phẩm vào danh sách yêu thích thành công.');
                                         // Thêm sản phẩm mới vào danh sách yêu thích hiển thị
                                         $scope.favorite.push(response.data);
-                                        showSuccessToast("Sản phẩm đã thêm vào danh sách thành công");
+                                        showSuccessToast("Sản phẩm đã thêm vào danh sách yêu thích thành công");
 
                                         $scope.fetchWishlistItems();
                                     })
-                                    .catch(function (error) {
+                                    .catch(function(error) {
                                         console.error('Lỗi khi thêm sản phẩm vào danh sách yêu thích:', error);
                                     });
                             }
                         })
-                        .catch(function (error) {
+                        .catch(function(error) {
                             console.error('Lỗi khi lấy sản phẩm theo Id:', error);
                         });
                 }
             }
-        }).catch(function (error) {
+        }).catch(function(error) {
             // Người dùng chưa đăng nhập hoặc có lỗi
             console.error('Lỗi đăng nhập hoặc chưa đăng nhập:', error);
             if (error === "Người dùng chưa đăng nhập") {
@@ -148,14 +148,14 @@ app.controller("WishlistController", function ($scope, ToastService, AccountServ
 
 
 
-    $scope.removeFromFavorite = function (event) {
-        AccountService.checkLogin().then(function (account) {
+    $scope.removeFromFavorite = function(event) {
+        AccountService.checkLogin().then(function(account) {
             // Người dùng đã đăng nhập
             $scope.account = account;
             if ($scope.account) {
                 var productId = event.currentTarget.getAttribute('data-product-id');
                 var existingFavoriteIndex = false;
-                $scope.favorite.forEach(function (item) {
+                $scope.favorite.forEach(function(item) {
                     if (item.product.id === parseInt(productId)) {
                         existingFavoriteIndex = true;
                     }
@@ -165,7 +165,7 @@ app.controller("WishlistController", function ($scope, ToastService, AccountServ
 
 
                     WishlistService.removeFromWishlist(productId)
-                        .then(function () {
+                        .then(function() {
                             // Xử lý phản hồi từ máy chủ nếu cần
                             console.log('Xoá sản phẩm khỏi danh sách yêu thích thành công.');
 
@@ -175,13 +175,13 @@ app.controller("WishlistController", function ($scope, ToastService, AccountServ
                             // Lấy danh sách yêu thích mới sau khi đã xoá thành công
                             $scope.fetchWishlistItems();
                         })
-                        .catch(function (error) {
+                        .catch(function(error) {
                             console.error('Lỗi khi xoá sản phẩm khỏi danh sách yêu thích:', error);
                         });
 
                 }
             }
-        }).catch(function (error) {
+        }).catch(function(error) {
             // Người dùng chưa đăng nhập hoặc có lỗi
             console.error('Lỗi đăng nhập hoặc chưa đăng nhập:', error);
             if (error === "Người dùng chưa đăng nhập") {
@@ -222,12 +222,12 @@ app.controller("WishlistController", function ($scope, ToastService, AccountServ
             const toast = document.createElement("div");
 
             // Auto remove toast
-            const autoRemoveId = setTimeout(function () {
+            const autoRemoveId = setTimeout(function() {
                 main.removeChild(toast);
             }, duration + 1000);
 
             // Remove toast when clicked
-            toast.onclick = function (e) {
+            toast.onclick = function(e) {
                 if (e.target.closest(".toast__close")) {
                     main.removeChild(toast);
                     clearTimeout(autoRemoveId);
@@ -262,8 +262,8 @@ app.controller("WishlistController", function ($scope, ToastService, AccountServ
         }
     };
 
-}).filter('vndFormat', function () {
-    return function (input) {
+}).filter('vndFormat', function() {
+    return function(input) {
         if (!input) return '';
 
         // Chuyển đổi số về dạng chuỗi và đảm bảo nó là một số

6e8cbb85ffac14b33213ce4b3f38ec0ff6ecaf37
NguyenHan <handev2607@gmail.com>
2023-08-07 14:47:57 +0700

    Hân
    up code thống kê bên admin

---
+++ DATN_GameVH/src/main/java/com/gamevh/service/RevenueService.java
@@ -0,0 +1,11 @@
+package com.gamevh.service;
+
+import java.util.List;
+
+public interface RevenueService {
+	List<Object[]> getTotalOrderStatusByYear(Integer year);
+	
+	List<Object[]> getPaymentTypeStatsForMonth(Integer month);
+	
+	List<Object[]> getOrderStatsForMonthAndByCategory(Integer month);
+}
